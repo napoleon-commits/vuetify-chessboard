5add7d99f68543e2a3ef69536e81f87a7e5057d6
napoleon-commits <62509875+napoleon-commits@users.noreply.github.com>
2021-01-20 14:18:44 -0600

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <StaticChessBoard />
+  </v-container>
+</template>
+
+<script>
+  import StaticChessBoard from '@/components/subcomponents/StaticChessBoard';
+
+  export default {
+    name: 'HelloWorld',
+    components: {
+      StaticChessBoard,
+    },
+  }
+</script>
---
+++ src/components/subcomponents/StaticChessBoard.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-row>
+        <v-col>StaticChessBoard</v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
