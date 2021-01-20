4f2c5c2bf83983b7821d8ff97e206412228c4701
Byron Hall <byronhall@MacBook-Pro.attlocal.net>
2021-01-20 15:24:55 -0600

    added coordinates for black pieces and added functionality to toggle orientation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,35 @@
 <template>
   <v-container>
     <StaticChessBoard 
-      :pieceString="`rnbqkbnrpppppppp................................PPPPPPPPRNBQKBNR`"
-      :orientation="`white`"
+      :pieceString="pieceString"
+      :orientation="orientation"
     />
+    <div class="my-5">
+      <v-btn @click="toggleOrientation">Toggle Orientation</v-btn>
+    </div>
   </v-container>
 </template>
 
 <script>
   import StaticChessBoard from '@/components/subcomponents/StaticChessBoard';
+  import ReverseChessString from '@/utils/ReverseChessString';
 
   export default {
     name: 'HelloWorld',
     components: {
       StaticChessBoard,
+    },
+    data() {
+      return {
+        pieceString: 'rnbqkbnrpppppppp................................PPPPPPPPRNBQKBNR',
+        orientation: 'white',
+      };
+    },
+    methods: {
+      toggleOrientation() {
+        this.orientation = (this.orientation === 'white') ? 'black' : 'white';
+        this.pieceString = ReverseChessString(this.pieceString);
+      },
     },
   }
 </script>
