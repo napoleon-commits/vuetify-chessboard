63a780701ff819c5cd349f654a5a4a9cb0073c46
Byron Hall <byron.hall@ricardodefense.com>
2021-01-21 14:43:40 -0800

    added paddings to board

--- src/components/subcomponents/StaticChessBoard.vue
+++ src/components/subcomponents/StaticChessBoard.vue
@@ -1,13 +1,33 @@
 <template>
-    <div>
-        containerWidth: {{containerWidth}} ; containerHeight: {{containerHeight}}
+    <div
+        :style="`
+            padding-top: ${((containerHeight)-(containerHeight*GoldenRatio))/2}px;
+            padding-left: ${((containerWidth)-(containerWidth*GoldenRatio))/2}px;
+        `"
+    >
+        <div
+            :style="`
+                border: 2px solid black;
+                width: ${containerWidth*GoldenRatio}px;
+                height: ${containerHeight*GoldenRatio}px;
+            `"
+        >
+            containerWidth: {{containerWidth}} ; containerHeight: {{containerHeight}}
+        </div>
     </div>
 </template>
 
 <script>
+import GoldenRatio from '@/static/GoldenRatio';
+
 export default {
     name: 'StaticChessBoard',
     props: ['containerWidth', 'containerHeight'],
+    data() {
+        return {
+            GoldenRatio,
+        };
+    },
 }
 </script>
 
