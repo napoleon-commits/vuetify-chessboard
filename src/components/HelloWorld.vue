d19a91cd98cf7aa3101ffeddbfaad6a2801fbb32
Byron Hall <byron.hall@ricardodefense.com>
2021-01-21 13:30:20 -0800

    passed container dimensions to board component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,41 @@
 <template>
-  <div :style="`background-color: rgba(0,0,0,0.1); height: 100vh; width: 100vw;`">
-    <StaticChessBoard />
+  <div id="board-container" :style="`background-color: rgba(0,0,0,0.1); height: 100vh; width: 100vw;`">
+    <StaticChessBoard
+      :containerWidth="containerWidth"
+      :containerHeight="containerHeight"
+    />
   </div>
 </template>
 
 <script>
+  import $ from 'jquery';
   import StaticChessBoard from '@/components/subcomponents/StaticChessBoard';
 
   export default {
     name: 'HelloWorld',
     components: {
       StaticChessBoard,
+    },
+    data() {
+      return {
+        containerWidth: null,
+        containerHeight: null,
+      };
+    },
+    methods: {
+      updateContainerDimensions() {
+        this.containerWidth = $('#board-container').width();
+        this.containerHeight = $('#board-container').height();
+      },
+    },
+    mounted() {
+      this.$nextTick(()=>{
+        this.updateContainerDimensions();
+      });
+      window.addEventListener('resize', this.updateContainerDimensions);
+    },
+    beforeDestroy() {
+      window.removeEventListener('resize', this.updateContainerDimensions);
     },
   }
 </script>
--- src/components/subcomponents/StaticChessBoard.vue
+++ src/components/subcomponents/StaticChessBoard.vue
@@ -1,12 +1,13 @@
 <template>
-    <v-row>
-        <v-col>StaticChessBoard</v-col>
-    </v-row>
+    <div>
+        containerWidth: {{containerWidth}} ; containerHeight: {{containerHeight}}
+    </div>
 </template>
 
 <script>
 export default {
-
+    name: 'StaticChessBoard',
+    props: ['containerWidth', 'containerHeight'],
 }
 </script>
 
